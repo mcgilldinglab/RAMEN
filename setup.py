f84c41846f09ff725fb86c71656f1e29f7a66039
YWX2020 <yiwei.xiong@mail.mcgill.ca>
2023-01-25 21:09:42 +0000

    updated setup.py to include all subpackages

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(  name='RAMEN',
         author_email='yiwei.xiong@mail.mcgill.ca',
         url="https://github.com/mcgilldinglab/RAMEN",
         license='MIT',
-        packages=['ramen'],
+        packages=find_packages(include=['ramen', 'ramen.*']),
         install_requires=['pyitlib>=0.2.2','networkx>=2.8.6','numpy>=1.22.4','scikit-learn>=0.20,<0.23','matplotlib>=3.4.3', 'pandas>=1.3.3', 'igraph>=0.9.11'],
         classifiers=[
             'License :: OSI Approved :: MIT License',
