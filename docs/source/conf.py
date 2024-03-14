4a84715450e73a5ac441387abb145ada2f27e7ad
YWX2020 <yiwei.xiong@mail.mcgill.ca>
2024-03-13 20:39:05 -0400

    updating build files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,41 +1,83 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+r"""
+Sphinx configuration
+"""
+import os, sys
+import inspect
+import sphinx_autodoc_typehints
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-import os
-import sys
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 
-# -- Project information -----------------------------------------------------
+import scSemiProfiler
+project = 'scSemiProfiler'
+version = '1.0.0'
+release = '1.0.0'
+author = "Jingtao Wang"
+copyright = "McGill Ding Lab, 2023"
 
-project = 'RAMEN'
-copyright = '2024, Xiong, Yi Wei - Wang, Jing Tao - Ding, Jun'
-author = 'Xiong, Yi Wei - Wang, Jing Tao - Ding, Jun'
 
 
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
-    'sphinx.ext.mathjax']
+    'sphinx.ext.mathjax',
+    'sphinx_autodoc_typehints',
+    'sphinx_copybutton',
+    'nbsphinx'
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+
+intersphinx_mapping = dict(
+    python=('https://docs.python.org/3/', None),
+    numpy=('https://numpy.org/doc/stable/', None),
+    scipy=('https://docs.scipy.org/doc/scipy/reference/', None),
+    pandas=('https://pandas.pydata.org/pandas-docs/stable/', None),
+    sklearn=('https://scikit-learn.org/stable/', None),
+    matplotlib=('https://matplotlib.org/stable/', None),
+    seaborn=('https://seaborn.pydata.org/', None),
+    networkx=('https://networkx.org/documentation/stable/', None),
+    anndata=('https://anndata.readthedocs.io/en/stable/', None),
+    scanpy=('https://scanpy.readthedocs.io/en/stable/', None),
+    torch=('https://pytorch.org/docs/stable/', None),
+    ignite=('https://pytorch.org/ignite/', None),
+    plotly=('https://plotly.com/python-api-reference/', None)
+)
+
+qualname_overrides = {
+    'anndata._core.anndata.AnnData': 'anndata.AnnData',
+    'matplotlib.axes._axes.Axes': 'matplotlib.axes.Axes',
+    'networkx.classes.graph.Graph': 'networkx.Graph',
+    'networkx.classes.digraph.DiGraph': 'networkx.DiGraph',
+    'networkx.classes.multigraph.MultiGraph': 'networkx.MultiGraph',
+    'networkx.classes.multidigraph.MultiDiGraph': 'networkx.MultiDiGraph',
+    'numpy.random.mtrand.RandomState': 'numpy.random.RandomState',
+    'pandas.core.frame.DataFrame': 'pandas.DataFrame',
+    'scipy.sparse.base.spmatrix': 'scipy.sparse.spmatrix',
+    'seaborn.axisgrid.JointGrid': 'seaborn.JointGrid',
+    'torch.device': 'torch.torch.device',
+    'torch.nn.modules.module.Module': 'torch.nn.Module'
+}
+
+
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+
+
+source_suffix = '.rst'
+master_doc = 'index'
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store',\
+'scSemiProfiler.fast_generator.reparameterize_gaussian', 'scSemiProfiler.fast_generator.AdversarialTrainingPlan']
 
 html_show_sourcelink = True
 set_type_checking_flag = True
@@ -49,27 +91,9 @@ autodoc_default_options = {
     'autosummary': True
 }
 
-# Add any paths that contain templates here, relative to this directory.
-#templates_path = ['_templates']
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-autodoc_mock_imports = ["genetic_algorithm", "random_walk"]
-
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
+# -- Options for HTML output
 html_theme = 'sphinx_rtd_theme'
-autodoc_cache = False
 
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
